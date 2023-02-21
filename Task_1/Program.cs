60ea30cdb0e794f28ca042aa0df92a742b7e975b
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-22 00:38:19 +0600

    Task 2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите число А: ");
+int limit = Convert.ToInt32(Console.ReadLine());
+int GetSum(int A) // A = limit
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        sum += i;   // sum = sum + i;
+    }
+    return sum;
+}
+Console.WriteLine($"Cумма чисел от 1 до {limit} = {GetSum(limit)}");
+
---
+++ Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число");
+bool check = false;
+int number = 0;
+int count = 1;
+while (check == false)
+{
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out number))
+    {
+        for (int i = 0; i < number; i++)
+        {
+            number = number / 10;
+            count++;
+        }
+        break;
+        check = true;
+    }
+    else
+    {
+        Console.WriteLine("Не удалось распознать число, поробуйте ещё раз");
+    }
+}
+Console.WriteLine(count);
