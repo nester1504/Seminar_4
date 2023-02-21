78d6c0ab82acb30f0b83f6d2ec79a47b0fc7eec2
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-22 00:53:31 +0600

    Task 2.01

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -2,23 +2,27 @@
 // 456 -> 3
 // 78 -> 2
 // 89126 -> 5
-
 Console.WriteLine("Введите число");
 bool check = false;
 int number = 0;
-int count = 1;
+int count = 0;
 while (check == false)
 {
     string text = Console.ReadLine();
     if (int.TryParse(text, out number))
     {
-        for (int i = 0; i < number; i++)
+        // while (number != 0)
+        // {
+        //     number = number / 10;
+        //     count++;
+        // }
+        for (int i = 0; number > 0; i++)
         {
             number = number / 10;
             count++;
-        }
-        break;
-        check = true;
+        } 
+         break;      
+       
     }
     else
     {
